4c3c9545d560678ec3b1f72e86fe9e60c0ffd218
Juan-C13 <clavijuan.eg@gmail.com>
2023-03-11 12:28:16 -0500

    Respositorio 1

---
+++ Problema1.py
@@ -0,0 +1,7 @@
+#Juan Esteban Clavijo Garcia - Codigo: 202225709
+
+nombre=input("Digite su nombre:")
+estaturaM=float(input("Digite su estatura en metros:"))
+estaturaP=estaturaM*3.28084
+print("")
+print("Hola",nombre,", mides",estaturaP,"pies")
